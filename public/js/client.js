c839a3bedbb8a3c0a196e0944b669e59e4bc1007
Drew Siedel <dts.siedel@gmail.com>
2019-09-22 21:21:56 -0400

    Set initialization options for Cesium globe

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,16 @@
 function createViz(token) {
     Cesium.Ion.defaultAccessToken = token;
-    var viewer = new Cesium.Viewer('cesiumContainer');
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        animation: false,
+        baseLayerPicker: false,
+        fullscreenButton: false,
+        vrButton: false,
+        infoBox: false,
+        sceneModePicker: false,
+        selectionIndicator: false,
+        timeline: false,
+        shouldAnimate: false
+    });
 }
 
 function getToken() {
