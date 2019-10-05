429580a730ff1f8aeee891bac164ee07f320267c
Drew Siedel <dts.siedel@gmail.com>
2019-10-05 11:13:39 -0400

    Grab data from selected element, and parse date properly

--- public/js/client.js
+++ public/js/client.js
@@ -1,21 +1,79 @@
+var launchData;
 var selectedLaunch;
+const dateRegex = /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/;
 const flyOptions = {
-    duration: 1.5
+    duration: 2.5
 };
 
+//TODO: display list of launches, with selection
+//TODO (future): streetview embed based on lat/long
+
+// launchLibrary seems to return dates in a non-compliant version of ISO
+// that javascript can't recognize. This function tries to return a Date
+// object by slicing up the string. Returns null if it can't
+function parseLaunchDate(dateString) {
+    if (!dateString) { return null; }
+
+    var match = dateString.match(dateRegex)
+    if (!match) { return null; }
+    match = match.slice(1); // drop full match, keep capture groups
+    match[1] -= 1;          // month index offset :(
+    return new Date(...match);
+}
+
+// UUID generator pulled from:
+// https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+// given a uuid of a launch, grab the info map from the launchData, returning
+// null if it does not exist.
+function lookupById(uuid) {
+    var filtered = launchData.filter(l => {
+        return l.uuid === uuid
+    });
+    return (filtered.length < 1) ? null : filtered[0];
+}
+
+// Called whenever the user selects a new launch by any means. Sets the
+// selectedLaunch variable and flies the Cesium viewer to the selected
+// element.
+function select(viewer, element) {
+    if (element) {
+        selectedLaunch = lookupById(element._uuid);
+        console.log(selectedLaunch);
+        viewer.flyTo(element, flyOptions);
+    } else {
+        console.error("Selected element was null or undefined");
+    }
+}
+
 function displayLaunches(launchRaw, viewer) {
-    const launchLocations = launchRaw.launches.map(i => {
-        var location = i.location;
-        var pad = location.pads[0];
+    // extract useful information into a flat structure, and parse
+    // some pieces to be more useful. Sort by date from soonest to
+    // latest.
+    launchData = launchRaw.launches.map(launch => {
+        const location = launch.location;
+        const pad = location.pads[0];
+        const rocket = launch.rocket;
         return {
             lat: pad.latitude,
-            long: pad.longitude
+            long: pad.longitude,
+            launchName: launch.name,
+            padName: pad.name,
+            rocketName: rocket.name,
+            videoLink: launch.vidUrl,
+            timeStart: parseLaunchDate(launch.isostart)
         };
-    });
+    }).sort((a,b)=>a.timeStart-b.timeStart);
 
     const imageDimension = 80;
-    launchLocations.forEach(l => {
-        viewer.entities.add({
+    launchData.forEach(l => {
+        var entity = viewer.entities.add({
             position: Cesium.Cartesian3.fromDegrees(l.long, l.lat),
             billboard: {
                 image: "/images/rocket.png",
@@ -23,7 +81,7 @@ function displayLaunches(launchRaw, viewer) {
                 height: imageDimension
             },
             label: {
-                text: `${l.lat}, ${l.long}`,
+                text: l.rocketName,
                 font: "8pt monospace",
                 style: Cesium.LabelStyle.FILL_AND_OUTLINE,
                 outlineWidth : 2,
@@ -31,6 +89,9 @@ function displayLaunches(launchRaw, viewer) {
                 pixelOffset: new Cesium.Cartesian2(0, imageDimension/2)
             }
         });
+        var launchId = uuidv4();
+        entity._uuid = launchId;
+        l.uuid = launchId;
     });
 }
 
@@ -56,8 +117,7 @@ function createViz(token) {
         shouldAnimate: false
     });
     viewer.selectedEntityChanged.addEventListener((selected) => {
-        selectedLaunch = selected;
-        viewer.flyTo(selectedLaunch, flyOptions);
+        select(viewer, selected);
     });
     getLaunches(viewer);
 }
@@ -66,7 +126,7 @@ function getToken() {
     $.ajax({
         url: "/cesiumToken",
         success: createViz,
-        error: (_, __, error) => console.log(error)
+        error: (_, __, error) => console.error(error)
     });
 }
 
